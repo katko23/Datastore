13e39dd49bc1c6471479342567247a36142d9ef0
katko23 <catalin.coseru@isa.utm.md>
2022-11-09 10:14:10 +0200

    Some changes for 40 %

---
+++ Data_Buffer.py
@@ -0,0 +1,39 @@
+
+data_creation = []
+data_update = []
+data_delete = []
+data_received = []
+
+data = []
+def DataCreation(clientData):
+    if clientData['id'] == 666:
+        return 1
+    for d in data:
+        if d['id'] == clientData['id']:
+            return 1
+    data.append(clientData)
+    return 0
+
+
+def DataUpdate(clientData):
+    for d in data:
+        if d['id'] == clientData['id']:
+            d['data'] = clientData['data']
+            return 0
+    return 1
+
+def DataRead(clientData):
+    if clientData['id'] == 666:
+        return data
+    for d in data:
+        if d['id'] == clientData['id']:
+            return d
+    return 1
+
+def DataDelete(clientData):
+    datatemp = data.copy()
+    for d in datatemp:
+        if d['id'] == clientData['id']:
+            data.remove(d)
+            return 0
+    return 1
